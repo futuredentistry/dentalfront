b38987306c3f63ae96c292ba4e132520a4dbb394
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-04-02 18:24:44 +0400

    pager router structure

--- dentalfront/src/App.jsx
+++ dentalfront/src/App.jsx
@@ -3,24 +3,29 @@ import {
   BrowserRouter as Router, Route, Switch, Redirect,
 } from 'react-router-dom'
 
+import UnauthorizedRoute from 'modules/unauthorized_route'
+import * as ROUTES from 'modules/constants/routes'
+
 const App = () => {
   const authorized = true // ToDo configure auth
   return (
     <Router>
       <Switch>
+        {/*
+        ToDo by role if needed
         <Route
           exact
           path={ROUTES.LANDING}
-          render={props => (authorized() ? <Redirect to={ROUTES.USER} /> : <LandingPage {...props} />)}
-        />
+          render={props => (authorized ? <Redirect to={ROUTES.HOME} /> : <LandingPage {...props} />)}
+        /> */}
 
-        <UnauthorizedRoute path={ROUTES.GET_STARTED} authorized={authorized} component={GetStartedPage} />
-        <UnauthorizedRoute path={ROUTES.CREATE_ACCOUNT} authorized={authorized} component={CreateAccountPage} />
+        <Route exact path={ROUTES.HOME} component={Home} />
 
-        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route exact path={ROUTES.PASSWORD_FORGET} component={ForgetPasswordPage} />
+        <UnauthorizedRoute path={ROUTES.ADMIN} authorized={authorized} component={Admin} />
+        <UnauthorizedRoute path={ROUTES.DENTIST} authorized={authorized} component={Dentist} />
+        <UnauthorizedRoute path={ROUTES.PATIENT} authorized={authorized} component={Patient} />
+        <UnauthorizedRoute path={ROUTES.SCREENER} authorized={authorized} component={Screener} />
 
-        <PrivateRoute path={ROUTES.USER} authorized={authorized} component={UserRouterPage} />
         <Route component={() => <p>No Match</p>} />
       </Switch>
     </Router>
