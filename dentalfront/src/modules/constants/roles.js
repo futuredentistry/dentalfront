c2b455b05fd9c26e963de7d9a3f91ed78aac3c36
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-04-02 16:44:37 +0400

    added constants

---
+++ dentalfront/src/modules/constants/roles.js
@@ -0,0 +1,4 @@
+export const PATIENT = 'PATIENT'
+export const ADMIN = 'ADMIN'
+export const SCREENER = 'SCREENER'
+export const DENTIST = 'DENTIST'
---
+++ dentalfront/src/modules/constants/routes.js
@@ -0,0 +1,31 @@
+// Unauthorized
+export const HOME = '/'
+export const LOGIN = '/login'
+
+// Patient
+export const PATIENT = 'patient'
+export const PERSONAL = `${PATIENT}/personal`
+export const LIFESTYLE = `${PATIENT}/lifestyle`
+export const DENTAL = `${PATIENT}/dental`
+export const MEDICAL = `${PATIENT}/medical`
+export const SUMMARY = `${PATIENT}/summary`
+export const APPOINTMENT = `${PATIENT}/appointment`
+
+// Screener
+export const SCREENER = 'screener'
+export const REVIEW = `${SCREENER}/review`
+export const IMAGE_CAPTURE = `${SCREENER}/image_capture`
+export const SCREENER_COMPLETE = `${SCREENER}/completed`
+
+// Dentist
+export const DENTIST = 'DENTIST'
+export const PATIENT_REVIEW = `${DENTIST}/patient_review`
+export const DENTIST_CHART = `${DENTIST}/chart` // ToDO issue and image issue
+export const DENTAL_REPORT = `${DENTIST}/dental_report`
+export const DENTIST_COMPLETE = `${DENTIST}/completed`
+
+// Admin
+export const ADMIN = 'admin'
+export const ORGANISATION_REPORT = `${ADMIN}/_report`
+export const PATIENT_REPORT = `${ADMIN}/patient_report`
+export const DENTIST_REPORT = `${ADMIN}/dentist_report`
