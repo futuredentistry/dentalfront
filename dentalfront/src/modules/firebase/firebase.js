c0084a9eaf73e553ef3f7bbdd59044234fccdd47
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-04-05 22:34:26 +0400

    configured firebase

---
+++ dentalfront/src/modules/firebase/firebase.js
@@ -0,0 +1,99 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+}
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+
+    /* Helper */
+
+    this.serverValue = app.database.ServerValue
+    this.emailAuthProvider = app.auth.EmailAuthProvider
+
+    /* Firebase APIs */
+
+    this.auth = app.auth()
+    this.db = app.database()
+  }
+
+  // *** Auth API ***
+
+  // eslint-disable-next-line max-len
+  doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password)
+
+  // eslint-disable-next-line max-len
+  doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password)
+
+  doSignInAnonymously = () => this.auth.signInAnonymously()
+
+  doSignOut = () => this.auth.signOut()
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email)
+
+  doSendEmailVerification = () => this.auth.currentUser.sendEmailVerification({
+    url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+  })
+
+  // Return null or user object
+  getCurrentUser = () => this.auth.currentUser
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
+
+  // *** Merge Auth and DB User API *** //
+
+  onAuthUserListener = (next, fallback) => this.auth.onAuthStateChanged((authUser) => {
+    if (authUser) {
+      this.user(authUser.uid)
+        .once('value')
+        .then((snapshot) => {
+          const dbUser = snapshot.val()
+
+          if (dbUser) {
+            next({
+              uid: authUser.uid,
+              email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
+              ...dbUser,
+            })
+          } else next(authUser)
+        })
+    } else {
+      fallback()
+    }
+  })
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`)
+
+  // *** Change email/password API ***
+  reauthenticate = (currentPassword) => {
+    const user = this.auth.currentUser
+    const cred = this.emailAuthProvider.credential(user.email, currentPassword)
+
+    return user.reauthenticateAndRetrieveDataWithCredential(cred)
+  }
+
+  changePassword = (newPassword) => {
+    const user = this.auth.currentUser
+    return user.updatePassword(newPassword)
+  }
+
+  changeEmail = (newEmail) => {
+    const user = this.auth.currentUser
+    return user.updateEmail(newEmail)
+  }
+}
+
+export default Firebase
--- dentalfront/src/screens/Home/Home.jsx
+++ dentalfront/src/screens/Home/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import * as ROUTES from 'modules/constants/routes'
 
 const Home = () => (
-        <>
-            Home
-        </>
-    )
+    <>
+        Welcome to Beemo
+    </>
+)
 
 export default Home
