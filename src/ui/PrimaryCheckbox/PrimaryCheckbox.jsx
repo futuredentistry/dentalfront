8056a61f3aeabe8508524cdd284017350217b7ce
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-05-25 00:46:27 +0300

    justify lable

--- src/ui/PrimaryCheckbox/PrimaryCheckbox.jsx
+++ src/ui/PrimaryCheckbox/PrimaryCheckbox.jsx
@@ -1,23 +1,39 @@
-// @ts-nocheck
+
 import React from 'react'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles';
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
+import Typography from '@material-ui/core/Typography'
 
 import capitalizeFirstLetter from 'utils/capitalizeFirstLetter'
 
-const PrimaryCheckbox = ({ formLabel, formValue, onChange }) => (
-  <FormControlLabel
-    control={(
-      <Checkbox
-        checked={formValue}
-        onChange={() => onChange(!formValue)}
-        color="primary"
-      />
-    )}
-    label={capitalizeFirstLetter(formLabel)}
-  />
-)
+const useStyles = makeStyles(() => ({
+  justifyLabel: {
+    textAlign: 'justify',
+  },
+}))
+
+
+const PrimaryCheckbox = ({ formLabel, formValue, onChange }) => {
+  const classes = useStyles()
+  return (
+    <FormControlLabel
+      control={(
+        <Checkbox
+          checked={formValue}
+          onChange={() => onChange(!formValue)}
+          color="primary"
+        />
+      )}
+      label={
+        <Typography variant="body2" className={classes.justifyLabel}>
+          {capitalizeFirstLetter(formLabel)}
+        </Typography>
+      }
+    />
+  )
+}
 
 PrimaryCheckbox.propTypes = {
   formLabel: PropTypes.string.isRequired,
